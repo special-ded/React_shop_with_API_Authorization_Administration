d52d8d6bc79ed7950ecd20ac8a009c3c38edafe2
Ivan Draganov <draganov.ivan.an@gmail.com>
2023-06-23 11:04:51 +0200

    Sidebar Component created

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { CartContext } from "../../App";
+import localStorageService from "../../services/LocalStorage";
+import s from "./Sidebar.module.css";
+import admin from "../../assets/imgs/admin.png";
+
+export default function Sidebar() {
+  const { setToken } = useContext(CartContext);
+  function Logout() {
+    localStorageService.deleteToken();
+    setToken(null);
+  }
+
+  return (
+    <nav className={s.admin__navigation}>
+      <ul className={s.navigation__list}>
+        <li className={s.navigation__img}>
+          <img src={admin} className={s.img}></img>
+          <h4 className={s.admin__title}>Hi, Admin</h4>
+        </li>
+        <NavLink
+          to="/admin/products"
+          className={({ isActive, isPending }) =>
+            isPending ? "s.pending" : isActive ? `${s.active}` : ""
+          }
+        >
+          <li className={s.navigation__item}>Products</li>
+        </NavLink>
+        <NavLink
+          to="/admin/users"
+          className={({ isActive, isPending }) =>
+            isPending ? "s.pending" : isActive ? `${s.active}` : ""
+          }
+        >
+          {" "}
+          <li className={s.navigation__item}>Users</li>
+        </NavLink>
+        <NavLink
+          to="/admin/orders"
+          className={({ isActive, isPending }) =>
+            isPending ? "s.pending" : isActive ? `${s.active}` : ""
+          }
+        >
+          {" "}
+          <li className={s.navigation__item}>Orders</li>
+        </NavLink>
+        <li onClick={Logout} className={s.navigation__item}>
+          Log out
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -3,58 +3,17 @@ import s from "./Admin.module.css";
 import AdminProducts from "../../Components/AdminProducts/AdminProducts";
 import AdminOrders from "../../Components/AdminOrders/AdminOrders";
 import AdminUsers from "../../Components/AdminUsers/AdminUsers";
-import admin from "../../assets/imgs/admin.png";
+
 import { CartContext } from "../../App";
 import { useContext } from "react";
 import localStorageService from "../../services/LocalStorage";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 
 export default function Admin() {
-  const { setToken } = useContext(CartContext);
-  function Logout() {
-    localStorageService.deleteToken();
-    setToken(null);
-  }
-
   return (
     <section className={s.admin}>
       <div className={s.admin__wrapper}>
-        <nav className={s.admin__navigation}>
-          <ul className={s.navigation__list}>
-            <li className={s.navigation__img}>
-              <img src={admin} className={s.img}></img>
-              <h4 className={s.admin__title}>Hi, Admin</h4>
-            </li>
-            <NavLink
-              to="/admin/products"
-              className={({ isActive, isPending }) =>
-                isPending ? "s.pending" : isActive ? `${s.active}` : ""
-              }
-            >
-              <li className={s.navigation__item}>Products</li>
-            </NavLink>
-            <NavLink
-              to="/admin/users"
-              className={({ isActive, isPending }) =>
-                isPending ? "s.pending" : isActive ? `${s.active}` : ""
-              }
-            >
-              {" "}
-              <li className={s.navigation__item}>Users</li>
-            </NavLink>
-            <NavLink
-              to="/admin/orders"
-              className={({ isActive, isPending }) =>
-                isPending ? "s.pending" : isActive ? `${s.active}` : ""
-              }
-            >
-              {" "}
-              <li className={s.navigation__item}>Orders</li>
-            </NavLink>
-            <li onClick={Logout} className={s.navigation__item}>
-              Log out
-            </li>
-          </ul>
-        </nav>
+        <Sidebar />
         <div className={s.admin__products}>
           <Routes>
             <Route path="products" element={<AdminProducts />}></Route>
